453f06b4dc5938b54faad68c1f01d626110c1743
Sergey Yuferev <s.yuferev@boombate.com>
2012-06-21 18:54:47 +0400

    menu extension fixed

--- DependencyInjection/MillwrightMenuExtension.php
+++ DependencyInjection/MillwrightMenuExtension.php
@@ -19,8 +19,8 @@ class MillwrightMenuExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $processor = new Processor();
         $configuration = new Configuration();
+        $processor = new Processor();
         $config = $processor->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__
