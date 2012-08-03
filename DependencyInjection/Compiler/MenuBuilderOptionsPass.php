0dfcf9a39692b51c2c64eaa0f43bc919b2de1282
Stefan Zerkalica <zerkalica@gmail.com>
2012-08-03 10:18:41 +0400

    updates

--- DependencyInjection/Compiler/MenuBuilderOptionsPass.php
+++ DependencyInjection/Compiler/MenuBuilderOptionsPass.php
@@ -3,45 +3,54 @@ namespace Millwright\MenuBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Reference;
 
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\Config\Definition\Processor;
 
-use Millwright\ConfigurationBundle\DependencyInjection\Compiler\OptionCompilerPassBase;
+use Millwright\ConfigurationBundle\ContainerUtil;
 
 use Millwright\MenuBundle\DependencyInjection\MenuConfiguration;
 
-class MenuBuilderOptionsPass extends OptionCompilerPassBase
+class MenuBuilderOptionsPass implements CompilerPassInterface
 {
-    protected $optionBuilderId = 'millwright_menu.option.builder';
-    protected $optionsTag      = 'millwright_menu.menu_options';
-
-    protected function preProcess(array & $config, Processor $processor, ContainerBuilder $container)
+    public function process(ContainerBuilder $container)
     {
-        //reuse configuration for validating service-provided menu configs
-        $config = array('millwright_menu' => $config);
-        $config = $processor->processConfiguration(new MenuConfiguration, $config);
+        $normalizer = function(array &$config, Processor $processor, ContainerBuilder $container)
+        {
+            //reuse configuration for validating service-provided menu configs
+            $config = array('millwright_menu' => $config);
+            $config = $processor->processConfiguration(new MenuConfiguration, $config);
 
-        if(isset($config['renderers'])) {
-            $renderers = $config['renderers'];
-            foreach($config['tree'] as & $tree) {
-                if(isset($tree['type'])) {
-                    $type = $tree['type'];
-                    if(isset($renderers[$type])) {
-                        $renderOption = $renderers[$type];
-                        foreach(array('attributes') as $option) {
-                            if(isset($renderOption[$option])) {
-                                $tree[$option] = $renderOption[$option];
+            if(isset($config['renderers'])) {
+                $renderers = $config['renderers'];
+                foreach($config['tree'] as & $tree) {
+                    if(isset($tree['type'])) {
+                        $type = $tree['type'];
+                        if(isset($renderers[$type])) {
+                            $renderOption = $renderers[$type];
+                            foreach(array('attributes') as $option) {
+                                if(isset($renderOption[$option])) {
+                                    $tree[$option] = $renderOption[$option];
+                                }
                             }
                         }
                     }
                 }
             }
+        };
 
-            $container->getDefinition('millwright_menu.helper')
-                ->replaceArgument(2, $config['renderers']);
+        $config = ContainerUtil::collectConfiguration(
+            'millwright_menu.menu_options',
+            $container,
+            $normalizer
+        );
 
-            unset($config['renderers']);
-        }
+        $container->getDefinition('millwright_menu.helper')
+            ->replaceArgument(2, $config['renderers']);
+
+        unset($config['renderers']);
+
+        $container->getDefinition('millwright_menu.option.builder')
+            ->addMethodCall('setDefaults', array($config));
     }
 }
